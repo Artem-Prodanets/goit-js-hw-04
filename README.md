d9c231bb5296a3633b7995869fb2270943ae6e0e
Artem-Prodanets <a.prodanec@gmail.com>
2024-06-22 00:13:21 +0300

    task-1 done
    но вариантов несколько и что-то я запутался, нужно глянуть на свежую голову

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# goit-js-hw-03
+# goit-js-hw-04
 
-https://artem-prodanets.github.io/goit-js-hw-03/
+https://artem-prodanets.github.io/goit-js-hw-04/
--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,70 @@
-function slugify(title) { 
-    const lowerTitle = title.toLowerCase().trim();
-    const titleCut = lowerTitle.split(" ");
-    const titleJoin = titleCut.join("-");
-    return titleJoin;
+// * посчитать кол-во продуктов итого
+// * через иф сравнить итого с containerSize
+// * вернуть буль тру/фальс
+
+
+// * вариант на основании примера из обучения
+// function isEnoughCapacity(products, containerSize) {
+//   let totalProducts = 0;
+//   const values = Object.values(products);
+//   for (const value of values) {
+//     totalProducts += value;
+//   }
+//   if (totalProducts <= containerSize) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+
+// * мой вариант нерабочий
+// function isEnoughCapacity(products, containerSize) {
+//   let totalProducts = 0;
+//   for (const product in products) {
+//     totalProducts += Object.values(product);
+//   }
+//   if (totalProducts <= containerSize) {
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+// * вариант от чата, но я не помню чтоб мы учили "if (product in products)"
+// * вбудований оператор in, який перевіряє, чи є ключ прямо в об'єкті
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+
+  for (const product in products) {
+    if (product in products) {
+      totalProducts += products[product];
+    }
+  }
+  if (totalProducts <= containerSize) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 
-// * делим по пробелу
-// * преобразуем в малый регистр
-// * объединяем по тире
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
